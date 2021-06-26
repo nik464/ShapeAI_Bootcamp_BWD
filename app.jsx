4fd7f9b1b4a7961a4af8c5e60f098876ced74a0c
Nikhil Chaudhary <nikhil18chaudhary@gmail.com>
2021-06-26 20:11:26 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Footer from "./footer";
+import Header from "./header";
+import Info from "./info";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
---
+++ info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Bootcamp tutorial</h1>
+      <p>A basic web dev React js website made by Nikhil</p>
+    </div>
+  );
+}
+export default Info;
